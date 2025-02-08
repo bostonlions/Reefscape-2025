cd0789b5fb9204a44730276e816c233da683854e
mconine <maxconine@gmail.com>
2025-02-08 15:13:31 -0500

    Update Main.java

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -8,16 +8,18 @@ import edu.wpi.first.wpilibj.RobotBase;
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
- * you are doing, do not modify this file except to change the parameter class to the startRobot
- * call.
+ * you are doing, do not modify this file except to change the parameter class to the startRobot call.
  */
 public final class Main {
   private Main() {}
 
   /**
    * Main initialization function. Do not perform any initialization here.
-   *
    * <p>If you change your main robot class, change the parameter type.
+   * 
+   * Note: if deploying code isn't working, it might be because of vendordeps
+   * updates, in which case the code has to be built once first to allow for
+   * deploying in the future
    */
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
