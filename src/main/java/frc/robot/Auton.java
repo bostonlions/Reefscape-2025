7f963ec3a031fafdd46b8e0052a1a47d99af20ee
andyajohnson <82274010+andyajohnson@users.noreply.github.com>
2025-02-15 14:52:08 -0500

    Auton.java created and started

---
+++ src/main/java/frc/robot/Auton.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import frc.robot.subsystems.Trimmer;
+
+public final class Auton {
+    public Auton() {
+        initTrimmer();
+    }
+
+    private void initTrimmer() {
+
+    }
+}
